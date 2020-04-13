bfacd5130dac7e12dd9e52e9a142ecd69bd93c2f
Erin Ptacek <erin@latacora.com>
2020-04-12 19:26:13 -0500

    Here we go

---
+++ popup.js
@@ -0,0 +1,92 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+let goButton = document.getElementById('goButton');
+function htmlEscape(str, noQuotes) {
+  var map = [];
+  map['&'] = '&amp;';
+  map['<'] = '&lt;';
+  map['>'] = '&gt;';
+
+  var regex;
+
+  if (noQuotes) {
+    regex = /[&<>]/g;
+  }
+  else {
+    map['"'] = '&#34;';
+    map["'"] = '&#39;';
+    regex = /[&<>"']/g;
+  }
+
+  return ('' + str).replace(regex, function(match) {
+    return map[match];
+  });
+}
+
+function formatMessages(data) {
+  var str = '';
+  var blob = {};
+  try {
+    blob = JSON.parse(data);
+  }
+  catch(e) {
+    console.log(["Error parsing server response", e]);
+  }
+  finally {
+    if ( blob['ok'] === true ) {
+      str += '<table class="messages-table">';
+      str += '<thead>';
+      str += '<tr>';
+      str += '<th align="left">&nbsp;</th>';
+      str += '<th align="left">Channel</th>';
+      str += '<th align="left">Message</th>';
+      str += '</tr>';
+      str += '</thead>';
+      str += '<tbody>';
+      
+      for ( let [i, item] of Object.entries(blob['items']) ) {
+        str += '<tr>';
+        str += '<td class="radio"><input type="radio" id=" name="" value=""></td>';
+        for ( let [l, line] of Object.entries(item['messages']) ) {
+          console.log(line);
+          str += '<td>' + item['channel']['id'] + '</td>';
+          str += '<td>' + htmlEscape(line['text'], true) + '</td>';
+        }
+        str += '</tr>';
+      }
+      str += '</tbody></table>';
+    }
+  }
+  document.getElementById("messages").innerHTML = str;
+}
+
+function getMessages(user, token, url, teamId) {
+  console.log(url);
+  var req = new XMLHttpRequest();
+  var postData = 'module=messages&max_extract_len=9999&sort=score&query=from%3a%3c@' + user + '%3e&token=' + token + "&team=" + teamId
+  req.open("POST", url + 'api/search.modules', true);
+  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  req.onreadystatechange = function () {
+    if (req.readyState === 4 && req.status === 200) {
+    // XX ToDo(erin): catch not-JSON of failed response somewhere around here.
+      formatMessages(req.response);
+    }
+  };
+  req.send(postData);
+}
+
+goButton.onclick = function(element) {
+  chrome.tabs.executeScript({code: 'localStorage.getItem("localConfig_v2")'}, function(r) { 
+    let data = JSON.parse(r[0]);
+    var teamId = data['lastActiveTeamId'];
+    var teams = data['teams'];
+    var url = teams[teamId]['url'];
+    var token = teams[teamId]['token'];
+    var user = teams[teamId]['user_id'];
+    getMessages(user, token, url, teamId);
+ });
+};
